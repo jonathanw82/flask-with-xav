5525318d961390acda6cb9381460e4f692c1790b
jonathanw82 <jonwhewaycode@outlook.com>
2020-03-25 09:06:19 +0000

    initial push

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", page_title="Index Page")
+
+
+@app.route("/nextpage") 
+def nextpage():
+    return render_template("nextpage.html", page_title="Next Page")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', 5000)),
+            debug=True) 
